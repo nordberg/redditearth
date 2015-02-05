cce7e3e94fc6710c3665bade057e54a441009720
nordberg <marcusnordberg@live.se>
2015-02-05 23:49:19 +0100

    Download the image and upload it together with tweet (instead of just linking it)

--- postr.py
+++ postr.py
@@ -1,7 +1,10 @@
+from __future__ import print_function
 from twython import Twython
 import praw
 import time
 import datetime
+import requests
+import os
 
 APP_KEY = ''  # Customer Key here
 APP_SECRET = ''  # Customer secret here
@@ -9,14 +12,22 @@ OAUTH_TOKEN = ''  # Access Token here
 OAUTH_TOKEN_SECRET = ''  # Access Token Secret here
 
 r = praw.Reddit(user_agent='EarthPornLurker')
+if os.path.isfile('subm_pic.jpg'):
+	os.remove('subm_pic.jpg')
 while True:
-	top_subm = r.get_subreddit('earthporn').get_top_from_hour(limit=3)
+	top_subm = r.get_subreddit('EarthPorn').get_top_from_week(limit=10)
 	for subm in top_subm:
 		if 'reddit.com' not in subm.url:
-				status_to_post = '%s %s' %(subm, subm.url)
+			if 'imgur' in subm.url:
+				status_to_post = '%s %s %s' %(subm, subm.url, '#EarthPorn')
 				twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
-				twitter.update_status(status=str(status_to_post))
-				print 'Posted to twitter'
-				print 'Sleeping %s' %str(datetime.datetime.now())
-				time.sleep(60 * 60)
-		break
+				r = requests.get(subm.url)
+				with open('subm_pic.jpg', 'a') as the_file:
+					the_file.write(r.content)
+				image_open = open('subm_pic.jpg', 'r')
+				image_ids = twitter.upload_media(media=image_open)
+				twitter.update_status(status=str(status_to_post), media_ids=image_ids['media_id'])
+				print('Posted to twitter')
+				print('Sleeping %s' %str(datetime.datetime.now()))
+				time.sleep(60)
+				break			
